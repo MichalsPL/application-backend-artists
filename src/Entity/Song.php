428fa3c9159abfcd75e2b15a50ce6effd51eab86
Michal Strojek <michal.strojek@liberfinancegroup.com>
2018-11-14 20:42:46 +0100

    Test task creating entities and migrations

---
+++ src/Entity/Song.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SongRepository")
+ */
+class Song
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="dateinterval")
+     */
+    private $length;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Album", inversedBy="songs")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $album;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getLength(): ?\DateInterval
+    {
+        return $this->length;
+    }
+
+    public function setLength(\DateInterval $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function getAlbum(): ?Album
+    {
+        return $this->album;
+    }
+
+    public function setAlbum(?Album $album): self
+    {
+        $this->album = $album;
+
+        return $this;
+    }
+}
